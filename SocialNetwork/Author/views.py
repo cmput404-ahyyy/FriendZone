d3a04ee4b1dd83127a44d1461072d94b3bd78db8
ybekele <yonael@ualberta.ca>
2019-02-21 00:09:17 -0500

    added some html to login page

--- SocialNetwork/Author/views.py
+++ SocialNetwork/Author/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Author
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 # Create your views here.
 
 
 def login(request):
+    return render(request, 'author/login.html')
     pass
 
 
